97a34caa0de3395a004ce0965595d4005a2eba13
nourmami <nour27mami@gmail.com>
2022-10-02 22:28:32 +0100

    DTO added

---
+++ src/todo/DTO/ajout-todo.dto.ts
@@ -0,0 +1,5 @@
+export class AjoutTodoDto {
+
+        name: string;
+        description: string;
+}
---
+++ src/todo/DTO/update-todo.dto.ts
@@ -0,0 +1,8 @@
+export class UpdateTodoDto {
+    
+        id:string;
+        name:string;
+        description:string;
+   
+    
+}
